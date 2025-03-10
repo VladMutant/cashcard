53e3bbf15f19d15de8adb4deeaf54c4b233859b0
Vladyslav Chepchyn <vladyslav.chepchyn@student.tuke.sk>
2025-03-10 18:16:30 +0100

    add pagination and sorting support for cash card retrieval; enhance tests for new features

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,11 +1,15 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -26,7 +30,6 @@ class CashCardController {
         }
     }
 
-
     @PostMapping
     private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
         CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
@@ -35,5 +38,23 @@ class CashCardController {
                 .buildAndExpand(savedCashCard.id())
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+//    @GetMapping("/getAll")
+//    private ResponseEntity<Iterable<CashCard>> findAll(){
+//        Iterable<CashCard> allCashCards = cashCardRepository.findAll();
+//        return ResponseEntity.ok(allCashCards);
+//    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+//                        pageable.getSort(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
